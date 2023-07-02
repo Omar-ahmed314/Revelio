9565cb344b79ab11da556ca9081d675dea4e1d54
abbas <62177195+Omar-ahmed314@users.noreply.github.com>
2023-07-02 21:59:05 +0300

    create the landing page

---
+++ website/frontend/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import {Link} from 'react-router-dom'
+import '../styles/Home/Home.css'
+
+const Home = () => {
+
+    return (
+        <>
+        <div className="home_body">
+            <section className="background"></section>
+            <section className="page_content">
+                <section className="page_header">
+                    <ul>
+                        <li><a href="/">ABOUT US</a></li>
+                        <li><a href="/">DOCS</a></li>
+                        <li><a href="/">GITHUB</a></li>
+                    </ul>
+                </section>
+                <section className="intro_words">
+                    <div id="revelio_word">
+                        <h1>REVELIO.</h1>
+                    </div>
+                </section>
+                <section className="intro_btns">
+                    <a href="/">GET STARTED</a>
+                </section>
+            </section>
+        </div>
+        </>
+    )
+}
+
+export default Home
