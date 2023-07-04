da4a5fa71b45227962de9ab405896b4310cbb007
abbas <62177195+Omar-ahmed314@users.noreply.github.com>
2023-07-05 01:56:06 +0300

    drag and drop component

--- website/frontend/src/components/drag_drop.jsx
+++ website/frontend/src/components/drag_drop.jsx
@@ -1,17 +1,38 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import '../styles/Drag_Drop.css'
 
 const DragAndDrop = ({activateWindow}) => {
     const [isDraged, setDraged] = useState(false)
-    const dragDropContainer = useRef()
-    const fileDragedOver = () => {
+    const dragDropContainer = useRef(null)
+    const dragDropArea = useRef(null)
+
+    useEffect(() => {
+        dragDropArea.current.addEventListener('dragover', fileDragedOver)
+        dragDropArea.current.addEventListener('dragleave', fileDragedReleased)
+        dragDropArea.current.addEventListener('drop', fileDroped)
+    }, [])
+
+    const fileDragedOver = (event) => {
+        event.preventDefault()
         setDraged(true)
         dragDropContainer.current.classList.add('drag_over')
     }
+
     const fileDragedReleased = () => {
         setDraged(false)
         dragDropContainer.current.classList.remove('drag_over')
         }
+
+    const fileDroped = (event) => {
+        event.preventDefault()
+        const file = event.dataTransfer.files[0]
+    }
+
+    const readFileURL = (file) => {
+        const fileReader = new FileReader()
+        
+    }
+
     return (
         <div className="popup_window">
             <div className="dragdrop_container">
@@ -20,9 +41,7 @@ const DragAndDrop = ({activateWindow}) => {
                     <div className="close_btn_container">
                         <button className="close_btn" onClick={() => activateWindow?.setPopupActive(false)}></button>
                     </div>
-                    <div className="dragdrop" 
-                    onDragOver={() => fileDragedOver()}
-                    onDragLeave={() => fileDragedReleased()}>
+                    <div className="dragdrop" ref={dragDropArea}>
                         {isDraged ? (<p>Release the file</p>) : 
                         (<p>DRAG & DROP <br/>OR 
                             <label htmlFor='upload_video'> UPLOAD</label>
