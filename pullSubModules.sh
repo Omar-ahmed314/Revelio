b2282d0e7875f2a773382f0b292945fa3b448a6c
Ammar Mohamed Sobhi <ammarmohamed13@gmail.com>
2023-07-06 18:38:10 +0300

    integration

---
+++ pullSubModules.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd Facial-Landmarks-Detector
+git pull
+cd ..
+cd Revelio-LipsMovement
+git pull
+cd ..
+cd Dynamic-texture-analysis-for-detecting-fake-faces-in-video-sequences
+git pull
+cd ..
+cd FaceDetection
+git pull
+cd ..
+cd SBI2
+git pull
+cd ..
